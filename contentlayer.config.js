83c3ae545231b059ea4748d9c43eab49058d94a3
Abhishek Takawane <takawaneabhishek04@gmail.com>
2026-09-03 13:00:44 +0530

    Fix

---
+++ contentlayer.config.js
@@ -0,0 +1,59 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import remarkGfm from "remark-gfm";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
+const computedFields = {
+  path: {
+    type: "string",
+    resolve: (doc) => `/${doc._raw.flattenedPath}`,
+  },
+  slug: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
+  },
+};
+
+const Project = defineDocumentType(() => ({
+  name: "Project",
+  filePathPattern: "projects/**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    published: {
+      type: "boolean",
+      required: true,
+    },
+    title: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+    date: {
+      type: "date",
+    },
+    url: {
+      type: "string",
+    },
+    repository: {
+      type: "string",
+    },
+  },
+  computedFields,
+}));
+
+module.exports = makeSource({
+  contentDirPath: "content",
+  documentTypes: [Project],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypePrettyCode, { theme: "github-dark" }],
+      [rehypeAutolinkHeadings],
+    ],
+  },
+});
